e2e84270afbbdb8730e72348a7929774a5c23352
최지원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2021-11-02 10:32:41 +0900

    Upload myPlayList(html, css, js)

---
+++ MyPlayList/myPlayList.js
@@ -0,0 +1,17 @@
+window.onload = function {
+    var jsh1 = document.getElementById('Jsh1');
+    var jsh2 = document.getElementById('Jsh2');
+    var jsh3 = document.getElementById('Jsh3');
+
+    jsh1.addEventListener('mouseover', function(e){
+        jsh1.style.opacity = 0.5;
+    })
+}
+
+/*
+[====  까먹었거나 몰랐던 부분 작성용  ====]
+
+<JS에서의 window.onload>
+- https://wiserloner.tistory.com/380
+
+*/
